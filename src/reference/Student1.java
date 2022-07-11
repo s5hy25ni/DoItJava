f0e01c18bab96ac4c32ff2f7930ee75fc089eaa1
s5hy25ni <s5hy25ni@gmail.com>
2022-07-11 20:08:47 +0900

    ch05-6 reference END 7/11

---
+++ src/reference/Student1.java
@@ -0,0 +1,11 @@
+package reference;
+
+public class Student1 {
+	int studentID;
+	String stuedentName;
+	// 아래 4가지 변수를 Subject class로 빼낸 다음, Student도 간단하게 바꿈
+	int koreaScore;
+	int mathScore;
+	String koreaSubject;
+	String mathSubject;
+}
---
+++ src/reference/Student3.java
@@ -0,0 +1,8 @@
+package reference;
+
+public class Student3 {
+	int studenID;
+	String studentName;
+	Subject korean;
+	Subject math;
+}
